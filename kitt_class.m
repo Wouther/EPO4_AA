9e9b9dfda8ff6321334cc920a64306754e3b675f
Wouther <wouther.bons@gmail.com>
2015-05-08 23:52:52 +0200

    Added retry-option for getting KITT's status

--- kitt_class.m
+++ kitt_class.m
@@ -8,15 +8,33 @@ classdef kitt_class < handle
     
     methods
        
-        function status = get_status(self)
+        %Get KITT's status.
+        %Returns false when failed. Also returns number of tries needed.
+        %Can optionally retry when failed. Pass time (in milliseconds) to 
+        % wait between tries as first argument. Optional: maximum number of
+        % tries as second argument.
+        function [status, ntries] = get_status(self, varargin)
             global com;
             
-            status_original = com.get_status();
-            if ~isstruct(status_original) && status_original == false
-                disp('Error: getting status failed. ''status''-field unchanged.');
-                status = false;
-                return;
+            ntries = 0;
+            while true
+                status_original = false; %com.get_status();
+                if isstruct(status_original) %succeeded to get status
+                    break
+                end
+                ntries = ntries + 1;
+                
+                if nargin == 1 || ... %no retry
+                    (nargin == 3 && ntries == varargin{2}) %maximum n/o tries reached
+                    disp(['Error: getting status failed. Tried ' int2str(ntries) ...
+                        ' time(s). ''status''-field unchanged.']);
+                    status = false;
+                    return;
+                end
+                
+                pause(varargin{1}/1e3);
             end
+            
             status = self.normalize_status(status_original);
             self.status = status;
         end
